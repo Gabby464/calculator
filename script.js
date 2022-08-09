f2fb19acaf9db2b19d331ff6ee3cd267be5243f9
Gabriela P <gabriela.poryazova@gmail.com>
2022-08-09 21:28:49 +0300

    Store the values and operator for calculate function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const buttonElementsArray = Array.from(document.getElementsByClassName('buttons'));
 const displayEl = document.querySelector(".user-input");
+const buttonsDiv = document.querySelector(".buttons")
 
 buttonElementsArray.forEach((button) => {
     button.addEventListener('click', (event) => {
@@ -7,26 +8,53 @@ buttonElementsArray.forEach((button) => {
         const action = key.dataset.action;
         const keyContent = key.textContent;
         const displayedNum = displayEl.textContent;
+        const previousKeyType = buttonsDiv.dataset.previousKey;
+        // Remove .is-depressed class from all keys
+        Array.from(key.parentNode.children)
+            .forEach(k => k.classList.remove('is-depressed'))
 
-
-        //checks that detirmine the type of button clicked
+        //check how to fill the display depending on the key clicked;
         if (!action) {
-            console.log('number key')
-        }else if (
+            buttonsDiv.dataset.previousKey = 'number'
+            if (displayedNum === '0' || previousKeyType === 'operator') {
+                displayEl.textContent = keyContent
+            } else {
+                displayEl.textContent = displayedNum + keyContent
+            }
+        } else if (
             action === 'sum' ||
             action === 'subtract' ||
             action === 'multiply' ||
             action === 'divide'
         ) {
-            console.log('operator key!')
-        }else if(action === 'get-result'){
+            //remove functionality once an operator is selected
+            key.classList.add('is-depressed');
+            //adding attribute to buttonDiv showing that the last clicked was an operator
+            buttonsDiv.dataset.previousKey = 'operator'
+            //add the firstValue and operator as attributes
+            buttonsDiv.dataset.firstValue = displayedNum;
+            buttonsDiv.dataset.operator = action;
+
+        } else if (action === 'get-result') {
+            buttonsDiv.dataset.previousKey = 'resultPrint'
+            //get the values from the present attributes
+            const firstValue = buttonsDiv.dataset.firstValue;
+            const secondValue = displayedNum;
+            const operator = buttonsDiv.dataset.operator;
+            displayEl.textContent = calculate(firstValue, secondValue, operator)
             console.log('equal key')
-        }else if(action === 'clear'){
-            console.log('clear key')
-        }else if(action === 'go-back'){
-            console.log('go back key')
-        }else if(action === "decimal"){
-            console.log('decimal key')
+        } else if (action === 'clear') {
+            buttonsDiv.dataset.previousKey = 'clear'
+            displayEl.textContent = ''
+        } else if (action === 'go-back') {
+            buttonsDiv.dataset.previousKey = 'wentBack'
+            let temporaryArr = Array.from(displayedNum);
+            temporaryArr.pop()
+            displayEl.textContent = temporaryArr.join('')
+        } else if (action === "decimal") {
+            buttonsDiv.dataset.previousKey = 'decimal'
+            displayEl.textContent = displayedNum + '.'
         }
     })
 })
+
